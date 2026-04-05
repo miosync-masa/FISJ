b8009e1885fb6246196f7185993998d434de2ee6
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2026-04-05 14:58:06 +0900

    Create __init__.py

---
+++ FISJ/__init__.py
@@ -0,0 +1,8 @@
+from .main import NetworkAnalyzerCore, NetworkResult, DimensionLink, CooperativeEventNetwork
+
+__all__ = [
+    "NetworkAnalyzerCore",
+    "NetworkResult",
+    "DimensionLink",
+    "CooperativeEventNetwork",
+]
