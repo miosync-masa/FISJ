a721cd662d7982f5c2a6500a40c7c0292b9cb6ae
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2026-04-05 22:22:58 +0900

    Update __init__.py

--- FISJ/__init__.py
+++ FISJ/__init__.py
@@ -1,8 +1,11 @@
 from .main import NetworkAnalyzerCore, NetworkResult, DimensionLink, CooperativeEventNetwork
+from .adapter import FISJAdapter, MethodOutput
 
 __all__ = [
     "NetworkAnalyzerCore",
     "NetworkResult",
     "DimensionLink",
     "CooperativeEventNetwork",
+    "FISJAdapter",
+    "MethodOutput",
 ]
